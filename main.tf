ae165e3fb326e8ef401a5d0115b3a4361f380e29
sharawana <sharawana.official@gmail.com>
2025-07-22 15:12:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,135 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+# Use default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Use subnets in the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+
+# Security group for HTTP and SSH
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 instance 1
+resource "aws_instance" "web1" {
+  ami                         = ami-0062e0576ebcaa2a9
+  instance_type               = "t2.micro"
+  subnet_id                   = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "Hello from this side 1" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "Web1"
+  }
+}
+
+# EC2 instance 2
+resource "aws_instance" "web2" {
+  ami                         = ami-0062e0576ebcaa2a9
+  instance_type               = "t2.micro"
+  subnet_id                   = data.aws_subnets.default.ids[1]
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "Hello from the other side 2" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "Web2"
+  }
+}
+
+# Application Load Balancer
+resource "aws_lb" "alb" {
+  name               = "web-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.web_sg.id]
+  subnets            = [data.aws_subnets.default.ids[0], data.aws_subnets.default.ids[1]]
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "web-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path     = "/"
+    protocol = "HTTP"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web1" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.web1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "web2" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.web2.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+output "load_balancer_dns_name" {
+  value = aws_lb.alb.dns_name
+}
